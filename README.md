58cbac7a3866ea09dd9c06b49c2e843e47ed8ea1
Harsh Sinha <harshsin@iitk.ac.in>
2016-06-14 11:42:44 +0530

    taking down the page for now

--- README.md
+++
@@ -1,2 +0,0 @@
-# harshsinh
-my first attempt at self-tutoring myself web development and also my first page.
--- css/untitled.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main(){
-	int a=0 , b=0 , sum=0 ;
-	scanf("%d %d", &a, &b);
-	sum = a + b;
-	printf("%d",sum );
-	return 0;
-}
